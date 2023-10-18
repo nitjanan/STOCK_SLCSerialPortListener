08f7461ebfc72c2e3c94176bc01c9013fe6ffb7d
nitjanan <nitjan.rs@gmail.com>
2023-10-18 14:39:30 +0700

    expoert to express ss T3

--- SerialPortListener/MainForm.cs
+++ SerialPortListener/MainForm.cs
@@ -570,37 +570,26 @@ namespace SerialPortListener
                 //แสดงเลขน้ำหนักที่กำลังวิ่ง
 
                 string newString = tbData.Text.Remove(tbData.Text.LastIndexOf("\r"));
-                string remainingText = newString.Substring(newString.LastIndexOf("p"));
+                string remainingText = newString.Substring(newString.LastIndexOf("q"));
                 MatchCollection mc = Regex.Matches(remainingText, @"\d+");
 
                 if (mc.Count > 0)
                 {
-                    if (String.Compare(tbWeigtData.Text, mc[0].Value) != 0)
-                    {
-                        tbWeigtData.Text = mc[0].Value.TrimStart('0').PadLeft(1, '0');
-                        //tbWeigtData.ForeColor = Color.LightCoral;
-                    }
-                    else
-                    {
-                        tbWeigtData.ForeColor = Color.LightGreen;
-                    }
-
-                    /* stock หินใหญ่
                     //tbWeigtData.ForeColor = Color.LightGreen;
                     if (Int32.Parse(mc[0].Value) % 10 != 0 || Int32.Parse(mc[0].Value) > 100000)
                     {
                         //ไม่ต้องทำไร
                     }
-                    else if (Int32.Parse(mc[0].Value) < 10 ) { 
+                    else if (Int32.Parse(mc[0].Value) < 10)
+                    {
                         tbWeigtData.Text = "0";
                         //tbWeigtData.ForeColor = Color.LightGreen;
-                    }   
+                    }
                     else if (String.Compare(tbWeigtData.Text, mc[0].Value) != 0)
                     {
-                        tbWeigtData.Text = mc[0].Value.TrimStart('0').PadLeft(1, '0');
+                        tbWeigtData.Text = mc[0].Value;
                         //tbWeigtData.ForeColor = Color.LightCoral;
                     }
-                    */
 
                 }
             }
