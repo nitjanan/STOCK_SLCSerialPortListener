c05a1b1ca413ba46ff98fcfce9f2dc38320ce917
nitjanan <nitjan.rs@gmail.com>
2023-10-24 09:49:32 +0700

    fix set time after read weight

--- SerialPortListener/MainForm.cs
+++ SerialPortListener/MainForm.cs
@@ -641,8 +641,10 @@ namespace SerialPortListener
                 calculateWeight();
                 _spManager.StartListening();
 
+                dtWeightInTime.Text = DateTime.Now.ToShortTimeString();
+
                 //disable after read in
-                if(!Globals.isPermissionTop())
+                if (!Globals.isPermissionTop())
                     disableBtAfterRead(1);
             }
             catch (Exception) {
@@ -869,8 +871,10 @@ namespace SerialPortListener
                 calculateWeight();
                 _spManager.StartListening();
 
+                dtWeightOutTime.Text = DateTime.Now.ToShortTimeString();
+
                 //disable after read out
-                if(!Globals.isPermissionTop())
+                if (!Globals.isPermissionTop())
                     disableBtAfterRead(2);
             }
             catch (Exception) {
