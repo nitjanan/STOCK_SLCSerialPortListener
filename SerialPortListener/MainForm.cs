a431a7078fbc52bee1d9170aad1340b0a3a093e5
nitjanan <nitjan.rs@gmail.com>
2023-10-27 14:54:19 +0700

    fix read port

--- SerialPortListener/MainForm.cs
+++ SerialPortListener/MainForm.cs
@@ -562,28 +562,25 @@ namespace SerialPortListener
             {
                 //แสดงเลขน้ำหนักที่กำลังวิ่ง
 
+
                 string newString = tbData.Text.Remove(tbData.Text.LastIndexOf("\r"));
-                string remainingText = newString.Substring(newString.LastIndexOf("q"));
+                string remainingText = newString.Substring(newString.LastIndexOf("p"));
                 MatchCollection mc = Regex.Matches(remainingText, @"\d+");
+
+
+                //MatchCollection mc = Regex.Matches(str, @"\d+");
 
                 if (mc.Count > 0)
                 {
-                    //tbWeigtData.ForeColor = Color.LightGreen;
-                    if (Int32.Parse(mc[0].Value) % 10 != 0 || Int32.Parse(mc[0].Value) > 100000)
+                    if (String.Compare(tbWeigtData.Text, mc[0].Value) != 0)
                     {
-                        //ไม่ต้องทำไร
-                    }
-                    else if (Int32.Parse(mc[0].Value) < 10)
-                    {
-                        tbWeigtData.Text = "0";
-                        //tbWeigtData.ForeColor = Color.LightGreen;
-                    }
-                    else if (String.Compare(tbWeigtData.Text, mc[0].Value) != 0)
-                    {
-                        tbWeigtData.Text = mc[0].Value;
+                        tbWeigtData.Text = mc[0].Value.TrimStart('0').PadLeft(1, '0');
                         //tbWeigtData.ForeColor = Color.LightCoral;
                     }
-
+                    else
+                    {
+                        tbWeigtData.ForeColor = Color.LightGreen;
+                    }
                 }
             }
             catch (Exception ex)
