e7b432593139b1505d4c2c59529ce5d11b24e752
nitjanan <nitjan.rs@gmail.com>
2024-10-01 15:20:41 +0700

    uni ss

--- SerialPortListener/FPrint.cs
+++ SerialPortListener/FPrint.cs
@@ -73,6 +73,7 @@ namespace SerialPortListener
             //ps.PaperSize.RawKind = (int)System.Drawing.Printing.PaperKind.Standard9x11;
 
             ps.Margins = new System.Drawing.Printing.Margins(46, 46, 60, 30);
+            ps.PaperSize = new System.Drawing.Printing.PaperSize("CustomType", 827, 550);
             this.reportViewer1.SetPageSettings(ps);
 
             this.reportViewer1.SetDisplayMode(Microsoft.Reporting.WinForms.DisplayMode.PrintLayout);
