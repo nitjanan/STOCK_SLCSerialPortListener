851d25e79b7f40b3ad0e1565233f2aee11d45269
nitjanan <nitjan.rs@gmail.com>
2025-05-06 09:38:26 +0700

    fix disable weight total

--- SerialPortListener/MainForm.cs
+++ SerialPortListener/MainForm.cs
@@ -713,8 +713,8 @@ namespace SerialPortListener
             {
                 tbWeightIn.Enabled = true;
                 tbWeightOut.Enabled = true;
-                tbWeightTotal.Enabled = true;
-                tbQ.Enabled = true;
+                tbWeightTotal.Enabled = false;
+                tbQ.Enabled = false;
             }
             else if (mode.Equals(4))//disable all
             {
@@ -733,8 +733,8 @@ namespace SerialPortListener
             else if (mode.Equals(999)) {
                 tbWeightIn.Enabled = true;
                 tbWeightOut.Enabled = true;
-                tbWeightTotal.Enabled = true;
-                tbQ.Enabled = true;
+                tbWeightTotal.Enabled = false;
+                tbQ.Enabled = false;
 
                 tbCarLicense.Enabled = true;
                 tbCarCity.Enabled = true;
