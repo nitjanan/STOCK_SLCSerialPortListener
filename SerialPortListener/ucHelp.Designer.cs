09ad0d3bfb5e90c2cfdeab462a9fd219a2a9da1b
Userpc <Userpc@USER>
2026-07-02 13:25:47 +0700

    เพิ่มการควบคุม com port ผ่าน ucHelp

--- SerialPortListener/ucHelp.Designer.cs
+++ SerialPortListener/ucHelp.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace SerialPortListener
+namespace SerialPortListener
 {
     partial class ucHelp
     {
@@ -30,10 +30,24 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.cboPort = new System.Windows.Forms.ComboBox();
+            this.lblBaud = new System.Windows.Forms.Label();
+            this.cboBaud = new System.Windows.Forms.ComboBox();
+            this.lblParity = new System.Windows.Forms.Label();
+            this.cboParity = new System.Windows.Forms.ComboBox();
+            this.lblDataBits = new System.Windows.Forms.Label();
+            this.cboDataBits = new System.Windows.Forms.ComboBox();
+            this.lblStopBits = new System.Windows.Forms.Label();
+            this.cboStopBits = new System.Windows.Forms.ComboBox();
+            this.txtDataReceived = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(172, 432);
@@ -44,6 +58,7 @@
             // 
             // label2
             // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(330, 394);
@@ -52,10 +67,160 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "Version 1.0";
             // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPort.Location = new System.Drawing.Point(50, 40);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(41, 21);
+            this.lblPort.TabIndex = 2;
+            this.lblPort.Text = "Port";
+            // 
+            // cboPort
+            // 
+            this.cboPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboPort.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboPort.FormattingEnabled = true;
+            this.cboPort.Location = new System.Drawing.Point(150, 37);
+            this.cboPort.Name = "cboPort";
+            this.cboPort.Size = new System.Drawing.Size(180, 29);
+            this.cboPort.TabIndex = 3;
+            // 
+            // lblBaud
+            // 
+            this.lblBaud.AutoSize = true;
+            this.lblBaud.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBaud.Location = new System.Drawing.Point(50, 80);
+            this.lblBaud.Name = "lblBaud";
+            this.lblBaud.Size = new System.Drawing.Size(51, 21);
+            this.lblBaud.TabIndex = 4;
+            this.lblBaud.Text = "Baud";
+            // 
+            // cboBaud
+            // 
+            this.cboBaud.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboBaud.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboBaud.FormattingEnabled = true;
+            this.cboBaud.Location = new System.Drawing.Point(150, 77);
+            this.cboBaud.Name = "cboBaud";
+            this.cboBaud.Size = new System.Drawing.Size(180, 29);
+            this.cboBaud.TabIndex = 5;
+            // 
+            // lblParity
+            // 
+            this.lblParity.AutoSize = true;
+            this.lblParity.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblParity.Location = new System.Drawing.Point(50, 120);
+            this.lblParity.Name = "lblParity";
+            this.lblParity.Size = new System.Drawing.Size(53, 21);
+            this.lblParity.TabIndex = 6;
+            this.lblParity.Text = "Parity";
+            // 
+            // cboParity
+            // 
+            this.cboParity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboParity.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboParity.FormattingEnabled = true;
+            this.cboParity.Location = new System.Drawing.Point(150, 117);
+            this.cboParity.Name = "cboParity";
+            this.cboParity.Size = new System.Drawing.Size(180, 29);
+            this.cboParity.TabIndex = 7;
+            // 
+            // lblDataBits
+            // 
+            this.lblDataBits.AutoSize = true;
+            this.lblDataBits.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDataBits.Location = new System.Drawing.Point(50, 160);
+            this.lblDataBits.Name = "lblDataBits";
+            this.lblDataBits.Size = new System.Drawing.Size(76, 21);
+            this.lblDataBits.TabIndex = 8;
+            this.lblDataBits.Text = "DataBits";
+            // 
+            // cboDataBits
+            // 
+            this.cboDataBits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboDataBits.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboDataBits.FormattingEnabled = true;
+            this.cboDataBits.Location = new System.Drawing.Point(150, 157);
+            this.cboDataBits.Name = "cboDataBits";
+            this.cboDataBits.Size = new System.Drawing.Size(180, 29);
+            this.cboDataBits.TabIndex = 9;
+            // 
+            // lblStopBits
+            // 
+            this.lblStopBits.AutoSize = true;
+            this.lblStopBits.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStopBits.Location = new System.Drawing.Point(50, 200);
+            this.lblStopBits.Name = "lblStopBits";
+            this.lblStopBits.Size = new System.Drawing.Size(71, 21);
+            this.lblStopBits.TabIndex = 10;
+            this.lblStopBits.Text = "StopBits";
+            // 
+            // cboStopBits
+            // 
+            this.cboStopBits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboStopBits.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboStopBits.FormattingEnabled = true;
+            this.cboStopBits.Location = new System.Drawing.Point(150, 197);
+            this.cboStopBits.Name = "cboStopBits";
+            this.cboStopBits.Size = new System.Drawing.Size(180, 29);
+            this.cboStopBits.TabIndex = 11;
+            // 
+            // txtDataReceived
+            // 
+            this.txtDataReceived.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDataReceived.BackColor = System.Drawing.Color.White;
+            this.txtDataReceived.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDataReceived.Location = new System.Drawing.Point(360, 37);
+            this.txtDataReceived.Multiline = true;
+            this.txtDataReceived.Name = "txtDataReceived";
+            this.txtDataReceived.ReadOnly = true;
+            this.txtDataReceived.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDataReceived.Size = new System.Drawing.Size(131, 189);
+            this.txtDataReceived.TabIndex = 12;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(54, 244);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(110, 35);
+            this.btnStart.TabIndex = 13;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(184, 244);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(110, 35);
+            this.btnStop.TabIndex = 14;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
             // ucHelp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtDataReceived);
+            this.Controls.Add(this.cboStopBits);
+            this.Controls.Add(this.lblStopBits);
+            this.Controls.Add(this.cboDataBits);
+            this.Controls.Add(this.lblDataBits);
+            this.Controls.Add(this.cboParity);
+            this.Controls.Add(this.lblParity);
+            this.Controls.Add(this.cboBaud);
+            this.Controls.Add(this.lblBaud);
+            this.Controls.Add(this.cboPort);
+            this.Controls.Add(this.lblPort);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "ucHelp";
@@ -70,5 +235,18 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.ComboBox cboPort;
+        private System.Windows.Forms.Label lblBaud;
+        private System.Windows.Forms.ComboBox cboBaud;
+        private System.Windows.Forms.Label lblParity;
+        private System.Windows.Forms.ComboBox cboParity;
+        private System.Windows.Forms.Label lblDataBits;
+        private System.Windows.Forms.ComboBox cboDataBits;
+        private System.Windows.Forms.Label lblStopBits;
+        private System.Windows.Forms.ComboBox cboStopBits;
+        private System.Windows.Forms.TextBox txtDataReceived;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
     }
 }
--- SerialPortListener/ucHelp.cs
+++ SerialPortListener/ucHelp.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
@@ -22,9 +22,135 @@ namespace SerialPortListener
                 return _instance;
             }
         }
+        private SerialPortListener.Serial.SerialPortManager _spManager;
+
         public ucHelp()
         {
             InitializeComponent();
+            this.Disposed += UcHelp_Disposed;
+        }
+
+        private void UcHelp_Disposed(object sender, EventArgs e)
+        {
+            if (_spManager != null)
+            {
+                _spManager.NewSerialDataRecieved -= _spManager_NewSerialDataRecieved;
+            }
+        }
+
+        public void SetSerialPortManager(SerialPortListener.Serial.SerialPortManager spManager)
+        {
+            _spManager = spManager;
+            if (_spManager == null) return;
+
+            var settings = _spManager.CurrentSerialSettings;
+
+            // Ensure baud rate collection has items
+            if (settings.BaudRateCollection == null || settings.BaudRateCollection.Count == 0)
+            {
+                foreach (int rate in new int[] { 110, 300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 38400, 57600, 115200 })
+                {
+                    settings.BaudRateCollection.Add(rate);
+                }
+            }
+
+            // Ensure PortNameCollection is populated
+            if (settings.PortNameCollection == null || settings.PortNameCollection.Length == 0)
+            {
+                settings.PortNameCollection = System.IO.Ports.SerialPort.GetPortNames();
+            }
+
+            // Populate Port ComboBox
+            cboPort.Items.Clear();
+            if (settings.PortNameCollection != null)
+            {
+                cboPort.Items.AddRange(settings.PortNameCollection);
+                if (!string.IsNullOrEmpty(settings.PortName) && cboPort.Items.Contains(settings.PortName))
+                {
+                    cboPort.SelectedItem = settings.PortName;
+                }
+                else if (cboPort.Items.Count > 0)
+                {
+                    cboPort.SelectedIndex = 0;
+                    settings.PortName = cboPort.Text;
+                }
+            }
+
+            // Bind Baud ComboBox
+            cboBaud.DataSource = settings.BaudRateCollection;
+            if (settings.BaudRate > 0)
+            {
+                cboBaud.SelectedItem = settings.BaudRate;
+            }
+
+            // Bind Parity ComboBox
+            cboParity.DataSource = Enum.GetValues(typeof(System.IO.Ports.Parity));
+            cboParity.SelectedItem = settings.Parity;
+
+            // Bind DataBits ComboBox
+            cboDataBits.DataSource = settings.DataBitsCollection;
+            cboDataBits.SelectedItem = settings.DataBits;
+
+            // Bind StopBits ComboBox
+            cboStopBits.DataSource = Enum.GetValues(typeof(System.IO.Ports.StopBits));
+            cboStopBits.SelectedItem = settings.StopBits;
+
+            // Wire UI changes to update settings
+            cboPort.SelectedIndexChanged += (s, ev) => { settings.PortName = cboPort.Text; };
+            cboBaud.SelectedIndexChanged += (s, ev) => { if (cboBaud.SelectedItem != null) settings.BaudRate = (int)cboBaud.SelectedItem; };
+            cboParity.SelectedIndexChanged += (s, ev) => { if (cboParity.SelectedItem != null) settings.Parity = (System.IO.Ports.Parity)cboParity.SelectedItem; };
+            cboDataBits.SelectedIndexChanged += (s, ev) => { if (cboDataBits.SelectedItem != null) settings.DataBits = (int)cboDataBits.SelectedItem; };
+            cboStopBits.SelectedIndexChanged += (s, ev) => { if (cboStopBits.SelectedItem != null) settings.StopBits = (System.IO.Ports.StopBits)cboStopBits.SelectedItem; };
+
+            // Subscribe to new incoming data
+            _spManager.NewSerialDataRecieved += _spManager_NewSerialDataRecieved;
+        }
+
+        private void _spManager_NewSerialDataRecieved(object sender, SerialPortListener.Serial.SerialDataEventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new EventHandler<SerialPortListener.Serial.SerialDataEventArgs>(_spManager_NewSerialDataRecieved), new object[] { sender, e });
+                return;
+            }
+
+            int maxTextLength = 2000; // maximum text length in text box
+            if (txtDataReceived.TextLength > maxTextLength)
+                txtDataReceived.Text = txtDataReceived.Text.Remove(0, txtDataReceived.TextLength - maxTextLength);
+
+            string str = System.Text.Encoding.ASCII.GetString(e.Data);
+            txtDataReceived.AppendText(str);
+            txtDataReceived.ScrollToCaret();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (_spManager != null)
+            {
+                try
+                {
+                    _spManager.StartListening();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error starting COM port: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (_spManager != null)
+            {
+                try
+                {
+                    _spManager.StopListening();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error stopping COM port: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
 
         private void ucHelp_Load(object sender, EventArgs e)
