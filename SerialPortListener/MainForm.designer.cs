e2eb7ad50ba17454968cb92590624e4146d7c27a
Userpc <Userpc@USER>
2026-07-31 14:02:40 +0700

    Auto Update from WebApp version 1.0

--- SerialPortListener/MainForm.designer.cs
+++ SerialPortListener/MainForm.designer.cs
@@ -1752,7 +1752,7 @@
             // serialSettingsBindingSource
             // 
             this.serialSettingsBindingSource.DataSource = typeof(SerialPortListener.Serial.SerialSettings);
-            // 
+            //
             // tbStoneTypeName
             // 
             this.tbStoneTypeName.Anchor = System.Windows.Forms.AnchorStyles.None;
