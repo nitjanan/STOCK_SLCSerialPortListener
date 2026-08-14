ed4d21d4efa786d14d3f4a3e072aec82d7536613
Userpc <Userpc@USER>
2026-08-14 10:16:22 +0700

    DownloadSettingAsync in MainForm

--- SerialPortListener/Properties/Resources.Designer.cs
+++ SerialPortListener/Properties/Resources.Designer.cs
@@ -113,6 +113,16 @@ namespace SerialPortListener.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap download {
+            get {
+                object obj = ResourceManager.GetObject("download", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap edit_file_24px {
             get {
                 object obj = ResourceManager.GetObject("edit_file_24px", resourceCulture);
