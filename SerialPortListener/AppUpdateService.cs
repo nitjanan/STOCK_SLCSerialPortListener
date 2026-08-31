1671d4688ad7778389b2c85aa31d32a9824c4e51
Userpc <Userpc@USER>
2026-08-31 13:29:48 +0700

    fix version

--- SerialPortListener/AppUpdateService.cs
+++ SerialPortListener/AppUpdateService.cs
@@ -34,7 +34,7 @@ namespace SerialPortListener
 
         public static async Task<AppReleaseInfo> GetLatestReleaseAsync(
             HttpClient client, string baseUrl, string accessToken,
-            string product = "Stock.SLC.W4.IN.Server") //Stock.SLC.W4.IN.Server , Stock.SLC.W4.IN.Client
+            string product = "Stock.SLC.W3.IN.Server") //Stock.SLC.W4.IN.Server , Stock.SLC.W4.IN.Client
         {
             client.DefaultRequestHeaders.Authorization =
                 new AuthenticationHeaderValue("Bearer", accessToken);
@@ -53,7 +53,7 @@ namespace SerialPortListener
             HttpClient client, string baseUrl, string accessToken,
             string machineName, string fromVersion, string toVersion, bool updateApplied,
             string weightStationCode = null, bool sqlApplied = false,
-            string product = "Stock.SLC.W4.IN.Server")
+            string product = "Stock.SLC.W3.IN.Server")
         {
             client.DefaultRequestHeaders.Authorization =
                 new AuthenticationHeaderValue("Bearer", accessToken);
